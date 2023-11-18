d2051c07551d6286dafbbaf20dc319c2cb7278bd
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-11-18 14:05:49 +0530

    designing of app is completed and logic is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+
+  const[items, setItems]=useState([
+    // {itemName:"item1", quantity:1, isSelected:false},
+    // {itemName:"item2", quantity:1, isSelected:true},
+    // {itemName:"item3", quantity:1, isSelected:false},
+  ])
+
+const [inputValue, setInputValue] = useState("");
+
+const [totalItemsQuantity, setTotalItemsQuantity] = useState()
+
+const handleAddButtonClick =() =>{
+  const newItem = {
+  itemName: inputValue,
+  quantity: 1,
+  isSelected: false
+  }
+  const newItems=[...items, newItem]
+setItems(newItems);
+setInputValue("");
+calculateTotal();
+}
+
+const selectFunction =(index)=>{
+const newItems = [...items];
+
+newItems[index].isSelected = !newItems[index].isSelected
+setItems(newItems);
+calculateTotal();
+}
+
+const onHandleIncrease = (index)=>{
+  const newItems = [...items];
+
+newItems[index].quantity ++;
+setItems(newItems);
+calculateTotal();
+}
+
+const onHandleDecrease = (index)=>{
+  const newItems = [...items];
+
+newItems[index].quantity --;
+setItems(newItems);
+calculateTotal();
+} 
+
+const calculateTotal=()=>{
+ const  totalItemsQuantity = items.reduce((total, item)=>{
+return total + item.quantity;
+ },0)
+setTotalItemsQuantity(totalItemsQuantity)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="container">
+
+  <div className="search-box">
+    <input type="text" 
+    value={inputValue} 
+    placeholder='Add a Shopping Item' 
+    onChange={(event)=>{setInputValue(event.target.value)}} />
+    <i class="fa-solid fa-plus" 
+    // onClick={(inputValue === "") ? (alert("Please Enter A Shopping Item")) : (handleAddButtonClick())} 
+    onClick={()=>handleAddButtonClick()}
+      ></i>
+   </div>
+
+
+  <div className="shopping-item-container">
+    {items.map((item, index) => (
+     
+     <div className="shopping-item">
+      <div className="item-name" onClick={()=>selectFunction(index)}>
+{item.isSelected ? ( 
+<>
+<span><i class="fa-regular fa-circle-check"></i></span>
+<span className="itemm">{item.itemName}</span>
+
+</>
+)
+:
+(
+  <>
+  <span><i class="fa-regular fa-circle"></i></span>
+<span className="itemm">{item.itemName}</span>
+
+</>
+)
+}
+</div>
+
+<div className="item-quantity">
+<i class="fa-solid fa-less-than" onClick={()=>onHandleDecrease(index)}></i>
+  {item.quantity}
+  <i class="fa-solid fa-greater-than" onClick={()=>onHandleIncrease(index)}></i>
+  </div> 
+</div>
+)
+)
+}
+
+<div className="total">
+  Total : {totalItemsQuantity}
+</div>
+</div>
+
+
+</div>
     </div>
   );
 }
 
 export default App;
+
+
